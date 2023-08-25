944c87fa39c3b5c22e6fd4218121833d9d50c14f
Jakub Kujawa <ZK_Jakub@proton.me>
2023-08-25 23:54:07 +0200

    Add positional encoding

--- models/LatentDiffusion/encoder.py
+++ models/LatentDiffusion/encoder.py
@@ -1,4 +1,8 @@
-from torch import nn
+import itertools
+import math
+
+import torch
+from torch import nn, Tensor
 
 from models.LatentDiffusion.attention import WordAttention
 
@@ -13,4 +17,23 @@ class CharacterEncoder(nn.Module):
         self.emb_dim = hidden_size
         self.max_seq_len = max_seq_len
 
-        # TODO
+
+class PositionalEncoder(nn.Module):
+    def __init__(self, d_model: int, d_emb: int) -> None:
+        super().__init__()
+
+        self.register_buffer(
+            "positional_encodings", self.get_positional_encoding(d_model, d_emb), False
+        )
+
+    def forward(self):
+        return self.positional_encodings().detach().requires_grad_(False)
+
+    @staticmethod
+    def get_positional_encoding(d_model: int, d_emb: int) -> Tensor:
+        encodings = torch.zeros(d_model, d_emb)
+        for pos, i in itertools.product(range(d_model), range(0, d_emb, 2)):
+            encodings[pos, i] = math.sin(pos / (10000 ** (i / d_emb)))
+            encodings[pos, i + 1] = math.cos(pos / (10000 ** (i / d_emb)))
+
+        return encodings
