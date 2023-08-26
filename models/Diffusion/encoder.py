Fix positional encoding calculation in PositionalEncoder for Diffusion Model

models/Diffusion/encoder.py
@@ -6,30 +6,59 @@ from torch import nn as nn, Tensor
 
 
 class PositionalEncoder(nn.Module):
-    def __init__(self, d_model: int, pos_factor: int = 1, dropout: int = 0.0) -> None:
+    def __init__(self, position: int, d_model: int, *, pos_factor: int = 1) -> None:
+        """
+        _summary_
+
+        Parameters
+        ----------
+        position : int
+            _description_
+        d_model : int
+            _description_
+        pos_factor : int, optional
+            _description_, by default 1
+        """
         super().__init__()
 
         self.d_model = d_model
         self.pos_factor = pos_factor
 
-        self.dropout = nn.Dropout(p=dropout)
+        self.register_buffer(
+            "positional_encodings",
+            self.get_positional_encoding(position, d_model),
+            False,
+        )
 
-    def forward(self, position: int) -> Tensor:
-        pe = self.__get_positional_encoding(position)
+    def forward(self) -> Tensor:
+        return self.positional_encodings.detach().requires_grad_(False)
 
-        return self.dropout(pe)
+    def get_positional_encoding(self, position: int, d_model: int) -> Tensor:
+        """
+        _summary_
 
-    def __get_positional_encoding(self, pos: int) -> Tensor:
-        position = rearrange(torch.arange(pos), "p -> p 1")
-        two_i = rearrange(torch.arange(self.d_model), "p -> 1 p")
+        Parameters
+        ----------
+        position : int
+            _description_
+        d_model : int
+            _description_
 
-        div_term = torch.exp(two_i * -(math.log(10000.0) / self.d_model))
+        Returns
+        -------
+        Tensor
+            _description_
+        """
+        position = rearrange(torch.arange(position), "p -> p 1")
+        two_i = rearrange(torch.arange(d_model), "p -> 1 p")
+
+        div_term = torch.exp(two_i * -(math.log(10000.0) / d_model))
         angle_rates = position * div_term * self.pos_factor
 
         angle_rates[:, 0::2] = torch.sin(angle_rates[:, 0::2])
         angle_rates[:, 1::2] = torch.cos(angle_rates[:, 1::2])
 
-        return rearrange(angle_rates, "b d -> 1 b d").requires_grad_(False)
+        return rearrange(angle_rates, "b d -> 1 b d")
 
 
 # class PositionalEncoder(nn.Module):

models/Diffusion/model.py
@@ -66,8 +66,10 @@ class AttentionalBlock(nn.Module):
         self.__device = device
 
         self.swap_channel_layer = swap_channel_layer
-        self.text_pos = PositionalEncoder(d_model)(2000).to(device)
-        self.stroke_pos = PositionalEncoder(d_model, pos_factor)(2000).to(device)
+        self.text_pos = PositionalEncoder(2000, d_model)().to(device)
+        self.stroke_pos = PositionalEncoder(2000, d_model, pos_factor=pos_factor)().to(
+            device
+        )
 
         self.dense_layer = nn.Linear(in_features=in_features, out_features=d_model)
         self.layer_norm = nn.LayerNorm(d_model, eps=1e-6, elementwise_affine=False)