2b799613848bab1cb0cdf91092f0ecd3fe26156e
Jakub Kujawa <ZK_Jakub@proton.me>
2023-09-10 22:41:31 +0200

    🐞 fix(activation): fix the RuntimeError error in the GeGLU activator
    The error occurred when the last dimension was an even number, not the other way around (it would not have occurred if the last dimension was not an even number)

--- models/LatentDiffusion/activation.py
+++ models/LatentDiffusion/activation.py
@@ -1,5 +1,5 @@
 """
-Originally copied from here, but modified a little bit for my taste.
+Originally copied from here, but modified a little bit.
 https://github.com/pfnet-research/deep-table/blob/master/deep_table/nn/layers/activation.py
 """
 
@@ -17,17 +17,63 @@ class GeGLU(nn.Module):
     """
 
     def __init__(self, d_in: int, d_out: int) -> None:
+        """
+        _summary_
+
+        Parameters
+        ----------
+        d_in : int
+            _description_
+        d_out : int
+            _description_
+        """
+
         super().__init__()
 
         self.proj = nn.Linear(d_in, d_out * 2)
 
     def forward(self, x: Tensor) -> Tensor:
-        if not x.shape[-1] % 2:
-            raise RuntimeError("The last dimension is not an even number")
+        """
+        _summary_
+
+        Parameters
+        ----------
+        x : Tensor
+            _description_
+
+        Returns
+        -------
+        Tensor
+            _description_
+
+        Raises
+        ------
+        RuntimeError
+            _description_
+        """
+
+        if x.shape[-1] % 2:
+            raise RuntimeError(
+                f"The last dimension ({x.shape[-1]}) is not an even number"
+            )
 
         return self.geglu(x)
 
     def geglu(self, x: Tensor) -> Tensor:
+        """
+        _summary_
+
+        Parameters
+        ----------
+        x : Tensor
+            _description_
+
+        Returns
+        -------
+        Tensor
+            _description_
+        """
+
         x, gate = self.proj(x).chunk(2, dim=-1)
 
         return x * F.gelu(gate)
