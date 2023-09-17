ac1531d5f6191b24bfef4523a105a2ef5e586801
Jakub Kujawa <ZK_Jakub@proton.me>
2023-09-17 20:15:10 +0200

    🦄 refactor(Diffusion): minor changes in the loss function

--- models/Diffusion/model.py
+++ models/Diffusion/model.py
@@ -344,10 +344,14 @@ class DiffusionModel(pl.LightningModule):
         strokes, strokes_pred, pen_lifts, pen_lifts_pred, alphas = loss_batch
 
         torch.clamp_(pen_lifts, min=1e-7, max=1 - 1e-7)
-        pen_lifts_pred = rearrange(torch.sigmoid(pen_lifts_pred), "b h 1 -> b h")
+        pen_lifts_pred = rearrange(pen_lifts_pred, "b h 1 -> b h")
         strokes_loss = torch.mean(torch.sum((strokes - strokes_pred) ** 2, dim=-1))
         pen_lifts_loss = torch.mean(
-            F.binary_cross_entropy_with_logits(pen_lifts_pred, pen_lifts)
+            reduce(
+                F.binary_cross_entropy(pen_lifts_pred, pen_lifts, reduction="none"),
+                "b v -> b",
+                "mean",
+            )
             * torch.squeeze(alphas, dim=-1)
         )
 
