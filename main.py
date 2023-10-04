d0f173fd7bf4ee4cb7fd0f8249a5e09c0ab5727c
Jakub Kujawa <ZK_Jakub@proton.me>
2023-10-04 13:13:39 +0200

    🐞 fix(utils): fix loading the processed dataset for the DM

--- main.py
+++ main.py
@@ -1,21 +1,33 @@
 import contextlib
+import datetime
 import json
+import math
 import os
 import random
 import sys
 from argparse import ArgumentParser
 from pathlib import Path
+from typing import List, Union
 
+import PIL.Image as ImageModule
 import lightning.pytorch as pl
 import neptune
+import numpy as np
 import torch
+import torchvision.transforms
 import yaml
+from PIL.Image import Image
+from einops import rearrange
 from lightning.pytorch.callbacks import (
     RichModelSummary,
     RichProgressBar,
     ModelCheckpoint,
+    EarlyStopping,
 )
 from lightning.pytorch.loggers.neptune import NeptuneLogger
+from matplotlib import pyplot as plt
+from torch.utils.data import DataLoader
+from torchmetrics.image.fid import FrechetInceptionDistance
 
 from configs.config import ConfigDiffusion, ConfigRNN, ConfigLatentDiffusion
 from data import utils
@@ -98,6 +110,19 @@ def cli_main():
         type=int,
         default=random.randint(0, 339),
     )
+    parser.add_argument(
+        "-s",
+        "--style_path",
+        help="Filename for style. If not provided, the default style is selected randomly",
+        type=str,
+        default=f"{os.listdir('assets')[np.random.randint(0, len(os.listdir('assets')))]}",
+    )
+
+    parser.add_argument(
+        "--fid",
+        help="Calculate FID between original and generated handwriting",
+        action="store_true",
+    )
 
     parser.add_argument("--train", help="Train selected model", action="store_true")
 
@@ -170,6 +195,8 @@ def prepare_data():
 
 def train_model():
     global dataset
+    patience = math.ceil(config.max_epochs / config.batch_size)
+    print(f"Patience set to {patience} epoch")
     # * ModelCheckpoint to save 9 (most of the time) latest models from epochs *
     trainer_params = dict(
         accelerator=config.device,
@@ -178,7 +205,7 @@ def train_model():
         callbacks=[
             RichModelSummary(max_depth=5),
             RichProgressBar(refresh_rate=1, leave=True),
-            # EarlyStopping(monitor="val_loss", patience=25),
+            EarlyStopping(monitor="val/loss", patience=patience),
             ModelCheckpoint(
                 dirpath=config.checkpoint_path,
                 monitor="val/loss",
@@ -250,6 +277,7 @@ def train_model():
 
     # * Only for university servers that have two GPUs *
     if config.device == "cuda" and args.remote:
+        torch.set_float32_matmul_precision("high")
         trainer_params["devices"] = [0]
 
     model = MODELS[args.config](**model_params)
@@ -257,8 +285,9 @@ def train_model():
     data_module = DataModule(config=config, dataset=dataset)
 
     if args.neptune:
-        source_files = ["main.py"] + [
-            str(path) for path in Path(f"models/{args.config}").glob("*.py")
+        source_files = [f"{os.getcwd()}/main.py"] + [
+            f"{os.getcwd()}/{str(path)}"
+            for path in Path(f"models/{args.config}").glob("*.py")
         ]
 
         neptune_logger = NeptuneLogger(
@@ -298,26 +327,148 @@ def train_model():
         model_version.stop()
 
 
-def generate_handwriting() -> None:
+def generate_handwriting() -> Union[Image, plt.Figure, List[Image], List[plt.Figure]]:
     model = MODELS[args.config].load_from_checkpoint(
         checkpoint_path=f"{config.checkpoint_path}/model.ckpt",
         map_location=torch.device(config.device),
     )
     model.eval()
 
-    save_path = f"{os.getcwd()}/images/{args.config}/{args.text.replace(' ', '-')}.jpeg"
+    save_path = (
+        None
+        if args.fid
+        else f"{os.getcwd()}/images/{args.config}/{args.text.replace(' ', '-')}.jpeg"
+    )
     if args.config == "LatentDiffusion":
         print(f"Selected writer id: {args.writer}")
-        model.generate(
+
+        return model.generate(
             args.text,
             vocab=config.vocab,
             writer_id=args.writer,
             save_path=save_path,
             color="black",
+            is_fid=args.fid,
         )
+
+    return model.generate(
+        args.text,
+        save_path=save_path,
+        vocab=config.vocab,
+        color="black",
+        style_path=args.style_path,
+        is_fid=args.fid,
+    )
+
+
+def full_sampling_for_fid():
+    # TODO: Implement IS score
+    # TODO: Implement for RNN models
+    fid = FrechetInceptionDistance(normalize=True)
+    fid.set_dtype(torch.float64)
+
+    device = torch.device(config.device)
+    if args.config == "LatentDiffusion":
+        normalize_undo = utils.NormalizeInverse((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+        fid_dataset = IAMDataset(
+            config=config,
+            img_height=config.img_height,
+            img_width=config.img_width,
+            max_text_len=config.max_text_len,
+            max_files=0,
+            dataset_type="train",
+        )
+
+        fid_dataloader = DataLoader(
+            fid_dataset,
+            batch_size=config.batch_size,
+            shuffle=False,
+            pin_memory=True,
+        )
+
+        for i, (writer_ids, images, labels) in enumerate(fid_dataloader, start=1):
+            print(
+                f"""
+            ===================================
+                Step {i}/{math.ceil(len(fid_dataset) / config.batch_size)}
+            ===================================
+                """
+            )
+            writer_ids, labels = writer_ids.to(device), labels.to(device)
+            real_samples = normalize_undo(images)
+            args.text, args.writer = labels, writer_ids
+
+            fake_samples = generate_handwriting()
+
+            fake_samples = [
+                rearrange(
+                    torchvision.transforms.ToTensor()(fake_sample), "c w h -> 1 c w h"
+                )
+                for fake_sample in fake_samples
+            ]
+            fake_samples = torch.cat(fake_samples, dim=0)
+
+            fid.update(real_samples, real=True)
+            fid.update(fake_samples, real=False)
+
+    elif args.config == "Diffusion":
+        fid_dataset = IAMonDataset(
+            config=config,
+            img_height=config.img_height,
+            img_width=config.img_width,
+            max_text_len=config.max_text_len,
+            max_seq_len=config.max_seq_len,
+            max_files=0,
+        )
+
+        fid_dataloader = DataLoader(
+            fid_dataset,
+            batch_size=config.batch_size,
+            shuffle=False,
+            pin_memory=True,
+        )
+
+        for i, (_, text, style, real_samples) in enumerate(fid_dataloader, start=1):
+            print(
+                f"""
+            ===================================
+                Step {i}/{math.ceil(len(fid_dataset) / config.batch_size)}
+            ===================================
+                """
+            )
+            text, style = text.to(device), style.to(device)
+            args.text, args.style_path = text, style
+
+            fake_samples = generate_handwriting()
+
+            fake_samples = [
+                ImageModule.frombytes(
+                    "RGB", fig.canvas.get_width_height(), fig.canvas.tostring_rgb()
+                )
+                for fig in fake_samples
+            ]
+
+            fake_samples = [
+                rearrange(torchvision.transforms.ToTensor()(image), "c h w -> 1 c h w")
+                for image in fake_samples
+            ]
+
+            fake_samples = torch.cat(fake_samples, dim=0)
+
+            fid.update(real_samples, real=True)
+            fid.update(fake_samples, real=False)
     else:
-        model.generate(
-            args.text, save_path=save_path, vocab=config.vocab, color="black"
+        raise NotImplementedError()
+
+    fid_value = fid.compute().item()
+    print(f"FID value: {fid_value}")
+
+    with open(f"{config.checkpoint_path}/metrics.txt", mode="w") as f:
+        f.write(
+            f"Date: {datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')} "
+            f"|| Model: {MODELS_SN[args.config]} "
+            f"|| FID value: {fid_value} "
+            f"|| IS value: None"
         )
 
 
@@ -340,5 +491,7 @@ if __name__ == "__main__":
         generate_handwriting()
     elif args.train:
         train_model()
+    elif args.fid:
+        full_sampling_for_fid()
     else:
         raise ValueError("The selected argument does not exist")
