:bug:: update transforms, model loading and accuracy computation
Transitioned to torchvision v2 transforms in both InceptionV3 and ConvNeXt models, and restructured the way models are loaded. Image processing was also enhanced in utils.py with an optimized normalization and format adjustment. Training and validation accuracy computation was tweaked to provide correct percentage values.

models/ConvNeXt/model.py
@@ -1,9 +1,9 @@
 import torch
 import torch.nn as nn
+import torchvision.transforms.v2 as transforms
 from torch import Tensor
-from torchvision import transforms, models
+from torchvision import models
 from torchvision.models import ConvNeXt_Tiny_Weights
-from torchvision.models.convnext import LayerNorm2d
 
 
 class ConvNeXt_M(nn.Module):
@@ -11,7 +11,7 @@ class ConvNeXt_M(nn.Module):
         super(ConvNeXt_M, self).__init__()
 
         self.num_class = num_class
-        self.transforms = transforms.Normalize(
+        self.normalize = transforms.Normalize(
             mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
         )
 
@@ -37,4 +37,4 @@ class ConvNeXt_M(nn.Module):
         )
 
     def forward(self, x: Tensor) -> Tensor:
-        return self.convnext(self.transforms(x))
+        return self.convnext(self.normalize(x))

models/ConvNeXt/utils.py
@@ -4,9 +4,14 @@ import torch
 import torch.nn as nn
 from einops import repeat
 from rich.progress import track
+from torch import Tensor
 from torch.optim import Optimizer
 from torch.optim.lr_scheduler import LRScheduler
 from torch.utils.data import DataLoader
+
+from data.utils import NormalizeInverse
+
+undo_normalize = NormalizeInverse((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
 
 
 class EarlyStopper:
@@ -88,8 +93,8 @@ def train_loop(
             scheduler.step()
 
         print(
-            f"Epoch [{epoch + 1}/{num_epochs}] Loss: {train_loss:.4f}, Accuracy: {train_acc:.4f}, "
-            f"Val loss: {val_loss:.4f}, Val accuracy: {val_acc:.4f}"
+            f"Epoch [{epoch + 1}/{num_epochs}] Loss: {train_loss:.4f}, Accuracy: {train_acc * 100:.4f}, "
+            f"Val loss: {val_loss:.4f}, Val accuracy: {val_acc * 100:.4f}"
         )
 
         if early_stopper is not None:
@@ -125,20 +130,21 @@ def validation(
         with torch.inference_mode():
             writer_id, image, _ = batch
             if train_config["model"] == "diffusion":
-                image = repeat(image, "b c h w -> b (c repeat) h w", repeat=3)
+                image = repeat(image, "b 1 h w -> b 3 h w")
+            else:
+                image = undo_normalize(image)
 
             image = image.to(device)
             writer_id = writer_id.type(torch.LongTensor).to(device)
 
             outputs = model(image)
-            pred_writer_id = torch.argmax(outputs, dim=1)
             loss = criterion(outputs, writer_id)
 
             val_loss += loss.item()
-            val_acc += torch.sum(pred_writer_id == writer_id.data)
+            val_acc += compute_accuracy(outputs, writer_id)
 
-    val_loss /= train_config["val_dataset_len"]
-    val_acc = val_acc.double().to("cpu") / train_config["val_dataset_len"]
+    val_loss /= len(val_loader)
+    val_acc /= len(val_loader)
 
     return val_loss, val_acc
 
@@ -157,7 +163,9 @@ def train(
     for batch in track(train_loader):
         writer_id, image, _ = batch
         if train_config["model"] == "diffusion":
-            image = repeat(image, "b c h w -> b (c repeat) h w", repeat=3)
+            image = repeat(image, "b 1 h w -> b 3 h w")
+        else:
+            image = undo_normalize(image)
 
         image = image.to(device)
         writer_id = writer_id.type(torch.LongTensor).to(device)
@@ -165,16 +173,20 @@ def train(
         optimizer.zero_grad()
 
         outputs = model(image)
-        pred_writer_id = torch.argmax(outputs, dim=1)
         loss = criterion(outputs, writer_id)
 
         loss.backward()
         optimizer.step()
 
         train_loss += loss.item()
-        train_acc += torch.sum(pred_writer_id == writer_id.data)
+        train_acc += compute_accuracy(outputs, writer_id)
 
-    train_loss /= train_config["train_dataset_len"]
-    train_acc = train_acc.double() / train_config["train_dataset_len"]
+    train_loss /= len(train_loader)
+    train_acc /= len(train_loader)
 
     return train_loss, train_acc
+
+
+def compute_accuracy(predictions: Tensor, labels: Tensor) -> float:
+    classes = torch.argmax(predictions, dim=1)
+    return torch.mean((classes == labels).float()).item()

models/InceptionV3/model.py
@@ -1,8 +1,8 @@
 import torch
 import torch.nn as nn
+import torchvision.transforms.v2 as transforms
 from torch import Tensor
-from torchvision import transforms
-from torchvision.models import Inception_V3_Weights
+from torchvision.models import Inception_V3_Weights, inception_v3
 
 
 class InceptionV3_M(nn.Module):
@@ -10,13 +10,11 @@ class InceptionV3_M(nn.Module):
         super(InceptionV3_M, self).__init__()
 
         self.num_class = num_class
-        self.transforms = transforms.Normalize(
+        self.normalize = transforms.Normalize(
             mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
         )
 
-        self.inception = torch.hub.load(
-            "pytorch/vision:v0.10.0",
-            "inception_v3",
+        self.inception = inception_v3(
             weights=Inception_V3_Weights.DEFAULT,
         )
 
@@ -45,4 +43,4 @@ class InceptionV3_M(nn.Module):
         self.inception.fc = nn.Linear(288, self.num_class)
 
     def forward(self, x: Tensor) -> Tensor:
-        return self.inception(self.transforms(x))
+        return self.inception(self.normalize(x))